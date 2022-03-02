f95817432d69ed027b813f3b1f8f1ace47207f02
Vanessa <hello@vfernandes.dev>
2022-03-02 17:22:58 +0100

    Add questions, conditions and conclusion

--- scripts/help-css.js
+++ scripts/help-css.js
@@ -11,6 +11,73 @@ let wrongB;
 let flashS;
 let flashR;
 
+const Game = {
+  currentLevel: 0,
+  levels: [
+    { correct: 1, answers: ["border-color: red", "border-color: green"] },
+    { correct: 1, answers: ["border-color: red", "border-color: green"] },
+    { correct: 1, answers: ["border-color: red", "border-color: green"] },
+  ],
+  isResetting: false,
+  canMove: false,
+  totalScore: 0,
+  hasAnswered: false,
+  hasUpdatedScore: false,
+};
+
+function startLevel() {
+  const actionElements = document.querySelectorAll(".action");
+  actionElements.forEach((elem) => {
+    elem.classList.add("hidden");
+  });
+
+  const allLevelElements = document.querySelectorAll(".level");
+  allLevelElements.forEach((elem) => {
+    elem.classList.add("hidden");
+  });
+  const levelElement = document.querySelector(`#level-${Game.currentLevel}`);
+  levelElement.classList.remove("hidden");
+
+  Game.isResetting = true;
+}
+
+function updateScore(isCorrect) {
+  const scoreElement = document.querySelector("#score-number");
+  Game.totalScore = Game.totalScore + (isCorrect ? 1 : 0);
+  scoreElement.innerText = Game.totalScore;
+
+  const allImageElements = document.querySelectorAll(".clara-image");
+  allImageElements.forEach((elem) => {
+    elem.classList.add("hidden");
+  });
+
+  const imageElement = document.querySelector(
+    `#clara-image-${Game.totalScore}`
+  );
+  imageElement.classList.remove("hidden");
+
+  const allStatusElements = document.querySelectorAll(".status");
+  allStatusElements.forEach((elem) => {
+    elem.classList.add("hidden");
+  });
+
+  const statusElement = document.querySelector(`#status-${Game.totalScore}`);
+  statusElement.classList.remove("hidden");
+
+  const actionElements = document.querySelectorAll(".action");
+  actionElements.forEach((elem) => {
+    elem.classList.add("hidden");
+  });
+
+  if (isCorrect) {
+    const correctButtonElement = document.querySelector(".nextLevel");
+    correctButtonElement.classList.remove("hidden");
+  } else {
+    const wrongButtonElement = document.querySelector(".wrong-answer");
+    wrongButtonElement.classList.remove("hidden");
+  }
+}
+
 // Basis functions
 function hidePage(name) {
   name.style.display = "none";
@@ -97,6 +164,8 @@ class Box {
     this.order = order;
     this.spaceLeft = spaceLeft;
     this.totalBoxesInRow = totalBoxesInRow;
+    this.isCollidingVert = false;
+    this.isCollidingHor = false;
   }
 
   get x1() {
@@ -108,7 +177,7 @@ class Box {
   }
 
   get y1() {
-    return canvasParent.clientHeight / 12;
+    return 0;
   }
 
   get y2() {
@@ -131,18 +200,78 @@ class Box {
 class NeutralBox extends Box {
   constructor(order, spaceLeft, totalBoxesInRow) {
     super(order, spaceLeft, totalBoxesInRow);
-    this.image;
+  }
+  isIn(x1, x2) {
+    return (this.x1 >= x1 && this.x2 <= x2) || (this.x1 <= x2 && this.x2 >= x1);
+  }
+
+  isInBetween(y1, y2) {
+    return (y1 <= this.y1 && this.y1 <= y2) || (y1 <= this.y2 && this.y2 <= y2);
+  }
+
+  collidesWithVert(box) {
+    return this.y1 <= box.y2 && this.isIn(box.x1, box.x2);
+  }
+
+  collidesWithHor(box) {
+    return (
+      this.x2 >= box.x1 &&
+      this.isInBetween(box.y1, box.y2) &&
+      this.x1 <= box.x2 &&
+      this.isInBetween(box.y1, box.y2)
+    );
+  }
+
+  collideVert() {
+    this.isCollidingVert = this.collidesWithVert(flash);
+  }
+
+  collideHor() {
+    this.isCollidingHor = this.collidesWithHor(flash);
   }
 
   draw() {
+    this.collideVert();
+    this.collideHor();
+
+    const correct = Game.levels[Game.currentLevel].correct;
+    const isThisCorrect = correct === this.order - 1;
+    const isColliding = this.isCollidingHor && this.isCollidingVert;
+
+    if (isColliding) {
+      Game.hasAnswered = true;
+    }
+
+    if (isColliding && isThisCorrect) {
+      Game.canMove = false;
+
+      if (!Game.hasUpdatedScore) {
+        updateScore(isThisCorrect);
+        Game.hasUpdatedScore = true;
+      }
+    }
+
+    if (isColliding && !isThisCorrect) {
+      updateScore(false);
+    }
+
+    let textString = Game.levels[Game.currentLevel].answers[this.order - 1];
+    if (Game.hasAnswered) {
+      if (isThisCorrect) {
+        textString = "YEAH " + textString;
+      } else {
+        textString = "NO " + textString;
+      }
+    }
+
     image(neutralB, this.x1, this.y1, this.width, this.height);
+    textSize(10);
+    text(textString, this.x1 + 5, this.y1 + 5, this.width - 5, this.height - 5);
   }
 }
 
-let neutralBox1 = new NeutralBox(1, canvasParent.clientWidth/9, 2);
+let neutralBox1 = new NeutralBox(1, canvasParent.clientWidth / 9, 2);
 let neutralBox2 = new NeutralBox(2, 0.055 * canvasParent.clientWidth, 2);
-
-console.log("0", neutralBox1.x1, neutralBox1.x2, neutralBox2.x1, neutralBox2.x2, canvasParent.clientWidth)
 
 class Player {
   constructor() {
@@ -152,7 +281,8 @@ class Player {
     this.offsetY = 0;
     this.acceleration = 0.3;
     this.isJumping = false;
-    this.isColliding = false;
+    this.isCollidingVert = false;
+    this.isCollidingHor = false;
   }
 
   get x1() {
@@ -189,12 +319,15 @@ class Player {
   }
 
   isIn(x1, x2) {
-    return ((this.x1 >= x1 && this.x2 <= x2) || (this.x1 <= x2 && this.x2 >= x1))
+    return (this.x1 >= x1 && this.x2 <= x2) || (this.x1 <= x2 && this.x2 >= x1);
+  }
 
+  isInBetween(y1, y2) {
+    return (y1 <= this.y1 && this.y1 <= y2) || (y1 <= this.y2 && this.y2 <= y2);
   }
 
   isInCanvas() {
-    return this.isIn(0, canvasParent.clientWidth)
+    return this.isIn(0, canvasParent.clientWidth);
   }
 
   isOnGround() {
@@ -202,55 +335,86 @@ class Player {
     else false;
   }
 
-  collidesWith(box) {
-    return (this.y1 <= box.y2 && this.isIn(box.x1, box.x2))
+  collidesWithVert(box) {
+    return this.y1 <= box.y2 && this.isIn(box.x1, box.x2);
   }
 
-  collide() {
-    this.isColliding = (this.collidesWith(neutralBox1) || this.collidesWith(neutralBox2))
+  collidesWithHor(box) {
+    return (
+      this.x2 >= box.x1 &&
+      this.isInBetween(box.y1, box.y2) &&
+      this.x1 <= box.x2 &&
+      this.isInBetween(box.y1, box.y2)
+    );
   }
 
+  collideVert() {
+    this.isCollidingVert =
+      this.collidesWithVert(neutralBox1) || this.collidesWithVert(neutralBox2);
+  }
+
+  collideHor() {
+    this.isCollidingHor =
+      this.collidesWithHor(neutralBox1) || this.collidesWithHor(neutralBox2);
+  }
+
+  isPressing(key) {
+    if (!Game.canMove) {
+      return false;
+    }
+    return keyIsDown(key);
+  }
 
   move() {
-
     if (this.isInCanvas()) {
-      
-      if (keyIsDown(LEFT_ARROW)) {
+      if (this.isPressing(LEFT_ARROW)) {
         this.speedX -= this.acceleration;
-      } 
-      if (keyIsDown(RIGHT_ARROW)) {
+      }
+      if (this.isPressing(RIGHT_ARROW)) {
         this.speedX += this.acceleration;
-      } 
-     } else {
-       if (this.x1 < 0) {
-         this.offsetX = 0;
-       } else {
-         this.offsetX = canvasParent.clientWidth - this.width;
-       }
-     }
-
-    if (!keyIsDown(LEFT_ARROW) && !keyIsDown(RIGHT_ARROW)) {
-      this.speedX = 0
+      }
+    } else {
+      if (this.x1 < 0) {
+        this.offsetX = 0;
+      } else {
+        this.offsetX = canvasParent.clientWidth - this.width;
+      }
     }
 
+    if (!this.isPressing(LEFT_ARROW) && !this.isPressing(RIGHT_ARROW)) {
+      this.speedX = 0;
+    }
 
-    if (((this.isOnGround() && !this.isJumping) || this.isJumping) && keyIsDown(UP_ARROW) && !this.isColliding) {
+    if (this.isCollidingHor) {
+      this.speedX = 0;
+      if (this.isPressing(RIGHT_ARROW)) {
+        this.speedX += this.acceleration;
+      }
+      if (this.isPressing(LEFT_ARROW)) {
+        this.speedX -= this.acceleration;
+      }
+    }
+
+    if (
+      ((this.isOnGround() && !this.isJumping) || this.isJumping) &&
+      this.isPressing(UP_ARROW) &&
+      !this.isCollidingVert
+    ) {
       this.isJumping = true;
       this.speedY -= this.acceleration;
-      console.log(this.offsetY);
     } else {
       this.isJumping = false;
       this.speedY += this.acceleration;
 
-      if (this.isColliding) {
+      if (this.isCollidingVert) {
         this.speedY = 0;
         this.speedY += this.acceleration;
       }
     }
-    
+
     this.offsetX += this.speedX;
-    this.offsetY += this.speedY;       
-    
+    this.offsetY += this.speedY;
+
     if (this.offsetY > 0) {
       this.speedY = 0;
       this.offsetY = 0;
@@ -283,9 +447,17 @@ function draw() {
   neutralBox1.draw();
   neutralBox2.draw();
 
-  flash.collide();
+  flash.collideHor();
+  flash.collideVert();
   flash.move();
   flash.draw();
+
+  if (Game.isResetting) {
+    Game.isResetting = false;
+    Game.canMove = true;
+    Game.hasAnswered = false;
+    Game.hasUpdatedScore = false;
+  }
 }
 
 //Here are "EventListener"
@@ -294,7 +466,14 @@ document.getElementById("start-button").onclick = () => {
   showPage(gamePageElement);
 };
 
+document.querySelector(".nextLevel").onclick = () => {
+  Game.currentLevel++;
+  startLevel();
+};
+
 window.addEventListener("onload", () => {
   hidePage(gamePageElement);
   hidePage(gameOverPageElement);
 });
+
+startLevel();
